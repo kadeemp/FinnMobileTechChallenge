baab0e37ac1b0748475f351ca2a6183b934ba951
Kadeem Palacios <kadeempalacios@gmail.com>
2018-03-20 02:48:01 -0700

    Added blank readme. Core Data Imlementation pt 2

---
+++ FinnMobileTechChallenge/Controllers/CoreData.swift
@@ -0,0 +1,42 @@
+//
+//  CoreData.swift
+//  FinnMobileTechChallenge
+//
+//  Created by Kadeem Palacios on 3/20/18.
+//  Copyright © 2018 Kadeem Palacios. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreData {
+
+    // MARK: - Core Data container setup
+    lazy var persistentContainer: NSPersistentContainer = {
+
+        let container = NSPersistentContainer(name: "AdCoreDataModel")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+
+}
--- FinnMobileTechChallenge/Services/adService.swift
+++ FinnMobileTechChallenge/Services/adService.swift
@@ -9,9 +9,12 @@
 import Foundation
 import SwiftyJSON
 import PromiseKit
+import CoreData
 
 struct adService {
     typealias ReturnAdsCompletion = (([Ad]) -> Void)
+
+
 
     static func loadAds(completion: @escaping ReturnAdsCompletion) -> [Ad] {
          var adObjectArray: [Ad] = []
@@ -43,9 +46,7 @@ struct adService {
 
                     completion(adObjectArray)
                 }
-
             }
-            print(adObjectArray)
         })
         return adObjectArray
 
@@ -61,58 +62,35 @@ struct adService {
 
     }
 
-//    static func loadAdsWithPromise() -> Promise<[Ad]> {
-//        return Promise{ fulfill  in
-//            NetworkingProvider.request(router: NetworkingRouter.getAds(), completionHandler: {(data, err) in
-//                let initQueue = DispatchQueue(label:"create_ad_object", qos: .userInitiated)
-//                initQueue.async {
-//                    let json = JSON(data!)
-//                    let jsonAdArray = json["items"].arrayValue
-//                    var adObjectArray:[Ad] = []
-//
-//                    //TODO: Change ad model init to parse data from there
-//                    // let ads = adArray.flatMap{Ad($0)}
-//
-//
-//                    for ad in jsonAdArray {
-//                        let location = ad[FinnAPI.adKeys.location].stringValue
-//                        let score = ad[FinnAPI.adKeys.score].doubleValue
-//                        let id = ad[FinnAPI.adKeys.id].intValue
-//                        let imageURL = ad[FinnAPI.adKeys.image][FinnAPI.adKeys.imageURL].stringValue
-//                        let adType = ad[FinnAPI.adKeys.adType].stringValue
-//                        let description = ad[FinnAPI.adKeys.description].stringValue
-//                        let type = ad[FinnAPI.adKeys.type].stringValue
-//                        let price = ad[FinnAPI.adKeys.price].intValue
-//                        let adObject = Ad(location: location, score: score, id: id, imageURL: imageURL, adType: adType, description: description, type: type, price: price)
-//                        //adObjectArray.append(adObject)
-//
-//                    }
-//                    DispatchQueue.main.async {
-//
-//                        //fulfill(adObjectArray)
-//                    }
-//
-//                }
-//        })
-//    }
-//    }
-    static func loadImage(imageURL:String) -> UIImage {
-
-        var image = UIImage()
-        NetworkingProvider.request(router: NetworkingRouter.getImages(path: imageURL), completionHandler: {(data, err) in
-            let initQueue = DispatchQueue(label:"create_ad_object", qos: .userInitiated)
-            initQueue.async {
-                image = UIImage(data: data as! Data)!
-
-            }
-        })
-        return image}
     
     static func imageURLConverter(imageUrlPath:String) -> URL {
         let baseURL = FinnAPI.imageBaseURL
         let imageUrlString = baseURL + imageUrlPath
         let imageURL = URL(string:imageUrlString)
         return imageURL!
+    }
+    static func saveAd(ad:Ad, image:UIImage) {
+        var coreData = CoreData()
+        var context:NSManagedObjectContext
+
+         let imageData = UIImageJPEGRepresentation(image, 1)
+            print("jpg conversion error")
+
+
+        var advertisement:NSManagedObject?
+        advertisement?.setValue(ad.location, forKey: "location")
+        advertisement?.setValue(ad.score, forKey: "score")
+        advertisement?.setValue(ad.id, forKey: "id")
+        advertisement?.setValue(imageData, forKey: "imageData")
+        advertisement?.setValue(ad.adType, forKey: "adType")
+        advertisement?.setValue(ad.description, forKey: "type")
+        advertisement?.setValue(ad.price, forKey: "price")
+
+        coreData.saveContext()
+
+
+
+        
     }
 }
 
